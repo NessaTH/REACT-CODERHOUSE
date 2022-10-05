5cadc36fbab7642669f3a28176caf03ec8b11239
Vanessa Tapia <tapiavanessa.vt@gmail.com>
2022-10-04 23:47:34 -0300

    GeschenkBox-Tapia (ItemCollectionDos)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,39 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/cartContext"
+import { collection, addDoc, getFirestore, connectFirestoreEmulator } from "firebase/firestore"
+import moment from "moment/moment";
 
 const Cart = () => {
 
     const {cart, addToCart, removeItem} = useContext(CartContext);
     console.log('cart', cart);
+
+    const createOrder = () =>{
+        const db = getFirestore();
+        const order = {
+            buyer: {
+                name: "Mey",
+                phone: "1122222222",
+                email: "testqtest.com"
+            },
+            items: cart,
+            total: cart.reduce((valorPasado, valorActual) => valorPasado + valorActual.price * valorActual.quantity , 0),
+            date: moment().format(),
+            
+        };
+        const query = collection(db, 'orders');
+        addDoc(query, order)
+        .then(({id}) =>{
+            console.log(id)
+            alert("Tu compra ha sido exitosa");
+        })
+        .catch((error) =>{
+            alert("tu compra no pudo ser procesada. Inténtalo de nuevo más tarde")
+        })
+    };
+
+
   return (<>
     <div>
         <h2 style={{color : "white"}}>Cart</h2>
@@ -25,17 +53,22 @@ const Cart = () => {
                             <p>{item.quantity}</p>
                             <button onClick={() => removeItem(item.id)}>Eliminar producto</button>
                         </div>
+                        
                     ))
-                }            
+                }   
+            {/* <div>
+                <button onClick={createOrder}>Crear orden</button>
+            </div>         */}
             </>
+            
         )}
 
     </div>
-
+          
   
   </>
     
-  )
-}
+  );
+};
 
 export default Cart;
